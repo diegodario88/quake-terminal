bdb05a6db649d1ba43291eb4d0e29189c2a341fa
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-09-24 16:35:09 -0300

    feat: add SignalConnector class and on/once functions
    This commit adds a SignalConnector class and two helper functions, 'on' and 'once', for working with signals (or events) in JavaScript. Signals allow objects to emit events, and the SignalConnector class helps manage event connections.
    
    The SignalConnector class represents a signal connection and provides a way to disconnect. The 'on' function is used to subscribe to a signal and receive continuous updates when the signal is emitted. The 'once' function is used to subscribe to a signal and automatically disconnect after the first emission.
    
    Additionally, this commit also adds a TERMINAL_STATE constant object with four symbols representing different states of a terminal: READY, STARTING, RUNNING, and DEAD.

--- quake-terminal@diegodario88.github.io/util.js
+++ quake-terminal@diegodario88.github.io/util.js
@@ -1,28 +1,94 @@
-export class Signal {
-	constructor(target, name, cb) {
+/**
+ * Signal Connector
+ *
+ * This module defines a SignalConnector class and functions 'on' and 'once' for working with signals (or events) in JavaScript.
+ * Signals allow objects to emit events, and the SignalConnector class helps manage event connections.
+ *
+ * - SignalConnector: A class that represents a signal connection. It connects to a target object's signal and provides a way to disconnect.
+ * - on(target, signalName, handler): Function to subscribe to a signal and receive continuous updates when the signal is emitted.
+ * - once(target, signalName, handler): Function to subscribe to a signal and automatically disconnect after the first emission.
+ *
+ * Example usage:
+ * const myObject = createObjectWithSignals(); // Replace with your own object
+ * const signalHandler = (signal, eventData) => {
+ *   console.log(`Signal '${signal.name}' emitted with data: ${eventData}`);
+ * };
+ *
+ * const connection = on(myObject, 'mySignal', signalHandler); // Subscribe to 'mySignal'
+ * // To disconnect later: connection.off();
+ *
+ * const oneTimeConnection = once(myObject, 'anotherSignal', signalHandler); // Subscribe to 'anotherSignal' for a single emission
+ *
+ * @class SignalConnector
+ * @module SignalConnector
+ */
+export class SignalConnector {
+	constructor(target, name, handler) {
 		this.name = name;
 		this.target = target;
-
-		this.id = target.connect(name, (...args) => cb(this, ...args));
+		this.id = target.connect(name, (...args) => handler(this, ...args));
 	}
+
+	/**
+	 * Disconnects a handler from an instance so it will not be called during
+	 * any future or currently ongoing emissions of the signal it has been
+	 * connected to.
+	 *
+	 * The `handler_id` becomes invalid and may be reused.
+	 *
+	 * @method off
+	 * @returns {void}
+	 */
 	off() {
 		this.target.disconnect(this.id);
 	}
 }
 
-export function on(target, signal_name, cb) {
-	return new Signal(target, signal_name, cb);
+/**
+ * Subscribe to a signal for continuous updates.
+ *
+ * @function on
+ * @param {object} target - The target object emitting the signal.
+ * @param {string} signalName - The name of the signal to subscribe to.
+ * @param {function} handler - The callback function to execute when the signal is emitted.
+ * @returns {SignalConnector} - A SignalConnector instance representing the connection.
+ */
+export function on(target, signalName, handler) {
+	const onSignal = new SignalConnector(target, signalName, handler);
+	return onSignal;
 }
 
-export function once(target, signal_name, cb) {
+/**
+ * Subscribe to a signal for a single emission, automatically disconnecting afterward.
+ *
+ * @function once
+ * @param {object} target - The target object emitting the signal.
+ * @param {string} signalName - The name of the signal to subscribe to.
+ * @param {function} handler - The callback function to execute when the signal is emitted.
+ * @returns {SignalConnector} - A SignalConnector instance representing the connection.
+ */
+export function once(target, signalName, handler) {
 	let disconnected = false;
-	return new Signal(target, signal_name, (signal, ...args) => {
+
+	const signalOnceHandler = (signal, ...args) => {
+		// Ensure we run the callback only once
 		if (disconnected) {
 			return;
 		}
 
 		disconnected = true;
-		signal.off();
-		cb(...args);
-	});
+		signal.off(); // Disconnect the signal
+		handler(...args);
+	};
+
+	const onceSignal = new SignalConnector(target, signalName, signalOnceHandler);
+
+	return onceSignal;
 }
+
+export const TERMINAL_STATE = {
+	READY: Symbol("READY"),
+	STARTING: Symbol("STARTING"),
+	RUNNING: Symbol("RUNNING"),
+	DEAD: Symbol("DEAD"),
+};
