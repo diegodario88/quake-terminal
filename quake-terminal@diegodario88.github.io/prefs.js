bc75abf1dbb358b43cd11acbc298237e810d0cb3
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2023-09-24 20:59:50 -0300

    style: lint

--- quake-terminal@diegodario88.github.io/prefs.js
+++ quake-terminal@diegodario88.github.io/prefs.js
@@ -4,74 +4,74 @@ import Gio from "gi://Gio";
 import Gtk from "gi://Gtk";
 
 export default class TogglerPreferences extends ExtensionPreferences {
-  fillPreferencesWindow(window) {
-    const settings = this.getSettings();
+	fillPreferencesWindow(window) {
+		const settings = this.getSettings();
 
-    const page = new Adw.PreferencesPage();
-    const group = new Adw.PreferencesGroup();
-    page.add(group);
+		const page = new Adw.PreferencesPage();
+		const group = new Adw.PreferencesGroup();
+		page.add(group);
 
-    // App ID
-    const rowId = new Adw.ActionRow({
-      title: "Terminal App ID",
-      subtitle: "/usr/share/applications/",
-    });
-    group.add(rowId);
+		// App ID
+		const rowId = new Adw.ActionRow({
+			title: "Terminal App ID",
+			subtitle: "/usr/share/applications/",
+		});
+		group.add(rowId);
 
-    const entryId = new Gtk.Entry({
-      placeholder_text: "org.gnome.Terminal.desktop",
-      text: settings.get_string("terminal-id"),
-      valign: Gtk.Align.CENTER,
-      hexpand: true,
-    });
+		const entryId = new Gtk.Entry({
+			placeholder_text: "org.gnome.Terminal.desktop",
+			text: settings.get_string("terminal-id"),
+			valign: Gtk.Align.CENTER,
+			hexpand: true,
+		});
 
-    settings.bind(
-      "terminal-id",
-      entryId,
-      "text",
-      Gio.SettingsBindFlags.DEFAULT
-    );
+		settings.bind(
+			"terminal-id",
+			entryId,
+			"text",
+			Gio.SettingsBindFlags.DEFAULT
+		);
 
-    rowId.add_suffix(entryId);
-    rowId.activatable_widget = entryId;
+		rowId.add_suffix(entryId);
+		rowId.activatable_widget = entryId;
 
-    // Shortcut
-    const rowShortcut = new Adw.ActionRow({
-      title: "Toggle Shortcut",
-      subtitle: "&lt;special_key&gt;regular_key",
-    });
-    group.add(rowShortcut);
+		// Shortcut
+		const rowShortcut = new Adw.ActionRow({
+			title: "Toggle Shortcut",
+			subtitle: "&lt;special_key&gt;regular_key",
+		});
+		group.add(rowShortcut);
 
-    const entryShortcut = new Gtk.Entry({
-      placeholder_text: "<Control>space",
-      text: settings.get_string("terminal-shortcut-text"),
-      valign: Gtk.Align.CENTER,
-      hexpand: true,
-    });
+		const entryShortcut = new Gtk.Entry({
+			placeholder_text: "<Super>Return",
+			text: settings.get_string("terminal-shortcut-text"),
+			valign: Gtk.Align.CENTER,
+			hexpand: true,
+		});
 
-    settings.bind(
-      "terminal-shortcut-text",
-      entryShortcut,
-      "text",
-      Gio.SettingsBindFlags.DEFAULT
-    );
+		settings.bind(
+			"terminal-shortcut-text",
+			entryShortcut,
+			"text",
+			Gio.SettingsBindFlags.DEFAULT
+		);
 
-    rowShortcut.add_suffix(entryShortcut);
-    rowShortcut.activatable_widget = entryShortcut;
+		rowShortcut.add_suffix(entryShortcut);
+		rowShortcut.activatable_widget = entryShortcut;
 
-    settings.connect("changed::terminal-shortcut-text", () => {
-      const shortcutText = settings.get_string("terminal-shortcut-text");
-      const [success, key, mods] = Gtk.accelerator_parse(shortcutText);
+		settings.connect("changed::terminal-shortcut-text", () => {
+			const shortcutText = settings.get_string("terminal-shortcut-text");
+			const [success, key, mods] = Gtk.accelerator_parse(shortcutText);
 
-      if (success && Gtk.accelerator_valid(key, mods)) {
-        const shortcut = Gtk.accelerator_name(key, mods);
-        settings.set_strv("terminal-shortcut", [shortcut]);
-      } else {
-        console.warn(`Invalid shortcut, ${shortcutText}`);
-        settings.set_strv("terminal-shortcut", []);
-      }
-    });
+			if (success && Gtk.accelerator_valid(key, mods)) {
+				const shortcut = Gtk.accelerator_name(key, mods);
+				settings.set_strv("terminal-shortcut", [shortcut]);
+			} else {
+				console.warn(`Invalid shortcut, ${shortcutText}`);
+				settings.set_strv("terminal-shortcut", []);
+			}
+		});
 
-    window.add(page);
-  }
+		window.add(page);
+	}
 }
